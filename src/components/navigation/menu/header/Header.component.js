30fa914b4bb64b8b1f7fc173d0a8d94f3701ef59
Miguel Suescun <debian789@gmail.com>
2018-05-15 15:47:45 -0500

    Se integra plugin react-native-i18n

--- src/components/navigation/menu/header/Header.component.js
+++ src/components/navigation/menu/header/Header.component.js
@@ -10,33 +10,18 @@ import HeaderStyle from './Header.style'
 import {connect} from 'react-redux'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {setContextNavigation} from '../../../../actions/navegation.action'
+import {strings} from '../../../../../locales/i18n'
 
 class HeaderComponent extends Component {
     constructor () {
-        super()
-        
+        super()        
     }
 
     render () {
         return (
              <View style={HeaderStyle.container}>
                  <View style={HeaderStyle.containerA}>
-                     <TouchableHighlight
-                          style={HeaderStyle.menu}
-                          onPress={() => {
-                              //debugger
-                              //if (this.props.navigation.navigation.state.index === 0) {
-                             //  debugger
-                             //debugger
-                                  this.props.navigation.navigation.navigate('DrawerOpen')
-                               //   this.props.navigation.navigate('DrawerOpen')
-                              //} else {
-                              //    this.props.navigation.navigation.navigate('DrawerClose')
-                              //}
-                          }}>
-                         <Text><Icon style={HeaderStyle.menuIcon} name="bars" size={30}/></Text>
-                     </TouchableHighlight>
-                     <View style={HeaderStyle.logo}><Text style={HeaderStyle.logoText}>Offrie</Text></View>
+                     <View style={HeaderStyle.logo}><Text style={HeaderStyle.logoText}>{strings('global.title')}</Text></View>
                      <TouchableHighlight
                           style={HeaderStyle.navegation}
                           onPress={() => {
@@ -54,20 +39,8 @@ class HeaderComponent extends Component {
                               //      this.props.navigation.navigate('DrawerClose');
                               // }
                           }}>
-                         <Text><Icon name="map" size={30} style={HeaderStyle.navegationIcon}/></Text>
+                         <Text><Icon name="search" size={30} style={HeaderStyle.searchIcon}/></Text>
                      </TouchableHighlight>
-                 </View>
-                 <View style={HeaderStyle.containerB}>
-                     <View style={HeaderStyle.searchContainer}>
-                         <View style={HeaderStyle.searchView}><Icon name="filter" size={30}
-                                                                    style={HeaderStyle.searchIcon}/></View>
-
-                         <View style={HeaderStyle.searchInput}><TextInput style={HeaderStyle.searchTextInput}
-                                                                          underlineColorAndroid='transparent'/></View>
-                         <View style={HeaderStyle.searchView}><Icon name="search" size={30}
-                                                                    style={HeaderStyle.searchIcon}/></View>
-
-                     </View>
                  </View>
              </View>
         )
