46ebb9259ad9f5daf293beb4fc47d95ba7659dcf
Miguel Suescun <debian789@gmail.com>
2018-05-21 19:24:11 -0500

    Soporte para el boton back

--- src/components/navigation/reduxNavigation.js
+++ src/components/navigation/reduxNavigation.js
@@ -1,7 +1,8 @@
-import {StackNavigator} from 'react-navigation'
+import {StackNavigator, NavigationActions} from 'react-navigation'
 import HeaderComponent from './menu/header/Header.component'
 import {connect} from 'react-redux'
 import React from 'react'
+import {BackHandler} from 'react-native'
 import PropTypes from 'prop-types'
 import {addListener} from '../../utils/redux'
 import AppNavigation from './appNavigation'
@@ -11,6 +12,23 @@ class AppWithNavigationState extends React.Component {
       dispatch: PropTypes.func.isRequired,
       nav: PropTypes.object.isRequired,
     };
+
+    componentDidMount() {
+      BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+    }
+    componentWillUnmount() {
+      BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+    }
+    onBackPress = () => {
+      const { dispatch, nav } = this.props;
+      if (nav.index === 0) {
+        return false;
+      }
+      dispatch(NavigationActions.back());
+      return true;
+    };
+
+    
   
     render() {
       const { dispatch, nav } = this.props;
