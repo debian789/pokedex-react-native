4881bdd811c0a7ccb08767693b8988b85984f1ef
Miguel Suescun <debian789@gmail.com>
2018-06-06 17:33:24 -0500

    Configuracion de estilos buscador!

--- src/components/navigation/menu/header/search/search.component.js
+++ src/components/navigation/menu/header/search/search.component.js
@@ -1,69 +1,71 @@
 import React, {Component} from 'react'
 import {View, Modal, Text, TextInput} from 'react-native'
 import {connect} from 'react-redux'
-import {active,desactive} from '../../../../../actions/search.action'
+import {active, desactive} from '../../../../../actions/search.action'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import SearchStyle from './search.style'
 import searchStyle from './search.style';
 
-
 class SearchComponent extends Component {
 
     _close() {
-        debugger
-        this.props.desactive()
+        this
+            .props
+            .desactive()
     }
 
     render() {
         return (
-        <View style={{flex:1}}>
-        <Modal
-          animationType="slide"
-          transparent={true}
+            <View style={{
+                flex: 1
+            }}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={this.props.search.state}
+                    onRequestClose={this
+                    ._close
+                    .bind(this)}>
+                    <View style={SearchStyle.container}>
+                        <View style={searchStyle.containerClose}>
+                            <Icon
+                                name="close"
+                                onPress={this
+                                ._close
+                                .bind(this)}
+                                size={25}
+                                style={SearchStyle.close}/></View>
 
-          visible={this.props.search.state}
-          onRequestClose={this._close.bind(this)}>
-          <View style={SearchStyle.container}>
-          <View style={searchStyle.containerClose}>
-          <Icon name="close"  onPress={this._close.bind(this)} 
-          size={25} style={SearchStyle.close}
-          
-              /></View>
+                        <View style={SearchStyle.containerText}>
+                            <Icon name="search" size={25} style={SearchStyle.searchIcon}/>
+                            <TextInput
+                                style={SearchStyle.input}
+                                onChangeText={() => {}}
+                                underlineColorAndroid='transparent'
+                                autoCorrect={false}/>
 
-          <View style={SearchStyle.containerText}>
-          <Icon name="search" size={25} style={SearchStyle.searchIcon}/>
-          <TextInput
-        style={SearchStyle.input}
-        onChangeText={() => {}}
-        underlineColorAndroid='transparent'
-        autoCorrect={false}
-        />
-      
-          </View>
-        </View>
+                        </View>
+                    </View>
 
-          </Modal>
-        </View>)
+                </Modal>
+            </View>
+        )
     }
 }
 
 const mapStateToProps = state => {
-    return {
-        nav: state.nav,
-        search: state.search
-    }
+    return {nav: state.nav, search: state.search}
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         active: () => {
-           return dispatch(active())
+            return dispatch(active())
         },
         desactive: () => {
             return dispatch(desactive())
         }
     }
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchComponent)
